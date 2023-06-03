2af1c232a834f4a797bf3596cb94dfb300626800
renatakov <renatakov1010@gmail.com>
2023-06-03 09:31:15 +0200

    -

--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -1,4 +1,22 @@
 const Home = () =>{
-    
+    return(
+        <>
+        <aside>
+            <nav>
+                <ul>
+                    <li>Men's fashion</li>
+                    <li>Woman's fashion</li>
+                    <li>Electronics</li>
+                    <li>Home & Lifestyle</li>
+                    <li>Medicine</li>
+                    <li>Sports & Outdoor</li>
+                    <li>Baby's & Toys</li>
+                    <li>Groceries & Pets</li>
+                    <li>Health & Beauty</li>
+                </ul>
+            </nav>
+        </aside>
+        </>
+    )
 }
 export default Home;
