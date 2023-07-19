f958e7c92c389576ef5405e5e3a56a54cd2332f2
RENATA_PRO <renatakov1010@gmail.com>
2023-07-19 17:55:05 +0200

    123

--- client/src/Components/Cart/Cart.jsx
+++ client/src/Components/Cart/Cart.jsx
@@ -1,11 +1,45 @@
 import Product from "../Product/Product"
 import s from "./Cart.module.css"
 const Cart = (props) => {
+  const basketItems = []
+  const idUser = sessionStorage.getItem("userAuthKey")
+  props.accounts.forEach((item) => {
+    if (idUser !== null) {
+      basketItems.push(...item.basket);
+    }
+  });
+    console.log(basketItems)
+    // props.products.forEach((item) => {
+    //     productsId.push(item.id);
+    // });
+    const arrWithPrices = []
+    const renderProducts = () => {
+      return basketItems.map((basketItem) => {
+        const product = props.products.find((product) => product.id === basketItem);
     
-        const arrWithPrices = []
-    //   const getPrice = props.cart.map((product)=>(
-    //     arrWithPrices.push(product.price)
-    //   ))
+        if (product) {
+
+          return <Product
+          id={product.id}
+          wishlist={props.wishlist}
+          key={product.id}
+          title={product.title}
+          img={product.img}
+          stars={product.stars}
+          sale={product.sale}
+          price={product.price}
+          reviews={product.reviews}
+          addToBasket={props.addToBasket}
+          accounts={props.accounts}
+          
+          />;
+        }
+        
+        
+      });
+    };
+    // console.log(productsId)
+
       console.log(arrWithPrices)
       const initialValue = 0
       const sumWithInitial = arrWithPrices.reduce(
@@ -18,10 +52,12 @@ const Cart = (props) => {
         <>
         <h1>Basket</h1>
         <div className={s.cartContainer}>
-        {/* {getCartItems} */}
+        {renderProducts()}
         </div>
-        <p>Total Sum: {sumWithInitial} $</p>
+        {/* <p>Total Sum: {sumWithInitial} $</p> */}
         </>
     )
 }
 export default Cart
+
+// 
